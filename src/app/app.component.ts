cd89dfc7a2de105cd3aef91aa7ec23dd45d0a2b2
yehonatan yehezkel <yoni333@gmail.com>
2024-01-29 09:54:19 +0200

    fix delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable, map } from 'rxjs';
-import { doc, docData, Firestore } from '@angular/fire/firestore';
-export interface contentItem {owner:string,title:string,date:number,datePirsum:number,link1:string,link2:string,link3:string,link4:string,link5:string,tag:string,text:string}
-export interface Item extends contentItem { id: string ,insertDate:number }
+export interface contentItem {owner:string,title:string,date:number,datePirsum:number,link1:string,link2:string,link3:string,link4:string,link5:string,tag:string,mainTag:string,text:string}
+export interface Item extends contentItem { customID: string ,insertDate:number }
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import {  FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 @Component({  
   selector: 'app-root',
  
@@ -12,9 +12,28 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/comp
 })
 export class AppComponent {
   public showLinks: boolean = false;
+  
   private itemsCollection: AngularFirestoreCollection<Item>;
+
+  public itemGroup:FormGroup
   items: Observable<Item[]>;
-  constructor(private readonly afs: AngularFirestore) {
+
+  constructor(private readonly afs: AngularFirestore,private readonly fb: NonNullableFormBuilder) {
+    this.itemGroup= this.fb.group({
+      customID:new FormControl<string>(''),
+      owner:new FormControl<string>('',[Validators.required,Validators.minLength(2),Validators.maxLength(15)],),
+      title:new FormControl<string>('',[Validators.required,Validators.minLength(2)]),
+      date:new FormControl<string>(''),
+      datePirsum:new FormControl<string>('',[Validators.required,Validators.minLength(2)]),
+      link1:new FormControl<string>(''),
+      link2:new FormControl<string>(''),
+      link3:new FormControl<string>(''),
+      link4:new FormControl<string>(''),
+      link5:new FormControl<string>(''),
+      tag:new FormControl<string>('',[Validators.minLength(2)]),
+      mainTag:new FormControl<string>('',[Validators.required,Validators.minLength(2),Validators.maxLength(15)]),
+      text:new FormControl<string>('',[Validators.required,Validators.minLength(10)]),
+  })
     this.itemsCollection = afs.collection<Item>('items');
     this.items = this.itemsCollection.valueChanges({ idField: 'customID' }).pipe(
       //sort items by datePirsum
@@ -22,15 +41,29 @@ export class AppComponent {
       
     )
   }
-  addItem(owner:string,title:string,date:string,datePirsum:string,link1:string,link2:string,link3:string,link4:string,link5:string,tag:string,text:string) {
+  addItem() {
     // Persist a document id
-    console.log(owner,title,date,datePirsum,link1,link2,link3,link4,link5,tag,text);
-    Number.parseInt(date);
-    const id = this.afs.createId();
-    const item: Item = { id, insertDate:new Date().valueOf(),owner,title,date:new Date(date).valueOf(),datePirsum:new Date(datePirsum).valueOf(),link1,link2,link3,link4,link5,tag,text };
-    console.log(item.date);
+    console.log(this.itemGroup);
+    console.log({a:'test',...this.itemGroup.value});
+    if (this.itemGroup.invalid){return}
+    let date  :string|number= this.itemGroup.controls['date'].value 
+    let datePirsum :string|number= this.itemGroup.controls['datePirsum'].value 
+    date =new Date(date?date:0).valueOf();
+    datePirsum =new Date(datePirsum?datePirsum:0).valueOf();
+    const customID = this.itemGroup.controls['customID'].value  ?this.itemGroup.controls['customID'].value: this.afs.createId();
+    const item: Item = { customID, insertDate:new Date().valueOf(),...this.itemGroup.value };
+   
     
-    this.itemsCollection.doc(id).set(item);
+    this.itemsCollection.doc(customID).set(item).then(res=>{
+      const name= this.itemGroup.controls['owner'].value
+      this.itemGroup.reset()
+      this.itemGroup.controls['owner'].setValue(name)
+    })
+  }
+  editItem(item:Item){
+    //insert each value in Item object to this.itemGroup
+    this.itemGroup.patchValue(item)
+    console.log("edit")
   }
   deleteItem(item:Item){
     console.log(item)
@@ -38,9 +71,8 @@ export class AppComponent {
   const r= confirm("למחק ידיעה ?")
   if (r===false ){return}
 
-              
 
-    this.itemsCollection.doc(item.id).delete();
+    this.itemsCollection.doc(item.customID).delete();
 
   }
 }
